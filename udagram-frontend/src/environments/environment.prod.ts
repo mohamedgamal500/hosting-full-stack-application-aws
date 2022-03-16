5141c500ab10c7c9e96b955118fe461da2fa906c
Mohamed Gamal <mohamedgamalmohamed800@gmail.com>
2022-03-16 21:30:51 +0200

    Update environment.prod.ts

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "http://udgram-api-dev.eba-e9pxbpzm.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
